782b799e131ff1ed23a40aefca61bde36920ee54
gulamc <gulamc@gmail.com>
2025-12-13 21:06:07 +0000

    Update layout file for browser heading

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next'
 import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Content Validator',
+  description: 'GRC Content Validator',
+}
 
 export default function RootLayout({
   children,
